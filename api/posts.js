16816912f8e1a57c911e3a51ec43659b125b43f8
Angel <angelortizsanchez383@gmail.com>
2023-10-03 23:49:58 -0700

    Finishing juicebox and renaming database to juiceboxdev since the original would not work

--- api/posts.js
+++ api/posts.js
@@ -38,9 +38,14 @@ postsRouter.get('/', async (req, res, next) => {
 });
 
 postsRouter.post('/', requireUser, async (req, res, next) => {
-  const { title, content = "" } = req.body;
+  const { title, content, tags = "" } = req.body;
 
+  const tagArr = tags.trim().split(/\s+/)
   const postData = {};
+
+  if (tagArr.length) {
+    postData.tags = tagArr;
+  }
 
   try {
     postData.authorId = req.user.id;
@@ -98,7 +103,26 @@ postsRouter.patch('/:postId', requireUser, async (req, res, next) => {
 });
 
 postsRouter.delete('/:postId', requireUser, async (req, res, next) => {
-  res.send({ message: 'under construction' });
+  try {
+    const post = await getPostById(req.params.postId);
+
+    if (post && post.author.id === req.user.id) {
+      const updatedPost = await updatePost(post.id, { active: false });
+
+      res.send({ post: updatedPost });
+    } else {
+      next(post ? { 
+        name: "UnauthorizedUserError",
+        message: "You cannot delete a post which is not yours"
+      } : {
+        name: "PostNotFoundError",
+        message: "That post does not exist"
+      });
+    }
+
+  } catch ({ name, message }) {
+    next({ name, message })
+  }
 });
 
 module.exports = postsRouter;
